fa9033d07e4cd2a48f219be0511d6df2abe777c6
Owale128 <Christian.buaka@hotmail.com>
2024-07-11 19:37:33 +0200

    Added useState and new functions in form component

--- src/app/rsvp/page.tsx
+++ src/app/rsvp/page.tsx
@@ -1,23 +1,59 @@
+'use client'
 import style from '../sass/Rsvp.module.scss';
-import React from 'react';
+import { Form } from '../model/Form';
+import React, { ChangeEvent, FormEvent, useState } from 'react';
                                                                 
 const RsvpPage = () => {
+    const [formData, setFormData] = useState<Form>({
+        firstName: '',
+        lastName: '',
+        guests: 0
+    })
 
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        const {name, value} = e.target
+        setFormData({...formData, [name]: value})
+    };
+
+    const handleSubmit = (e: FormEvent) => {
+        e.preventDefault()
+
+        setFormData({
+            firstName:'',
+            lastName:'',
+            guests: 0
+        })
+
+        console.log(formData)
+    }
     return(
         <div className={style.container}>
             <h1>RSVP</h1>
-            <form className={style.form}>
+            <form className={style.form} onSubmit={handleSubmit}>
                 <div className={style.formGroup}>
                     <label htmlFor="firstName">Firstname:</label>
-                    <input type="text" name='firstName' />
+                    <input type="text"
+                     name='firstName'
+                     value={formData.firstName}
+                     onChange={handleChange}
+                     />
                 </div>
                 <div className={style.formGroup}>
                     <label htmlFor="lastname">Lastname:</label>
-                    <input type="text" name='lastName' />
+                    <input type="text"
+                     name='lastName'
+                     value={formData.lastName}
+                     onChange={handleChange}
+                     />
                 </div>
                 <div className={style.formGroup}>
                     <label htmlFor="guests">Number of guests:</label>
-                    <input type="number" id='guests' name='guests' />
+                    <input 
+                    type="number"
+                     name='guests'
+                     value={formData.guests}
+                     onChange={handleChange}
+                      />
                 </div>
                 <button type='submit'>Submit</button>
             </form>
